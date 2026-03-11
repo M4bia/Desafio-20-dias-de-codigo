4cd05ba5939afaba278b4662a21ac537ec86467b
Mábia Silveira <mabiasilveira2424@gmail.com>
2026-03-10 23:45:35 -0300

    Dia 9
    Este programa lê três valores: o número de um funcionário, a quantidade de horas trabalhadas e o valor recebido por hora.
    Com essas informações, o programa calcula o salário total do funcionário, multiplicando as horas trabalhadas pelo valor por hora. Ao final, o programa imprime o número do funcionário e o salário calculado, formatado com duas casas decimais

---
+++ Dia 9.py
@@ -0,0 +1,8 @@
+numero = int(input())
+horas = int(input())
+valor_por_hora = float(input())
+
+salario = horas * valor_por_hora
+
+print("NUMBER =", numero)
+print("SALARY = U$ {:.2f}".format(salario))
